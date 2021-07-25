81349a3e064db509f4148da5223e651b2097bcd7
wojtek-lukowski <75492008+wojtek-lukowski@users.noreply.github.com>
2021-07-25 18:54:34 +0200

    Task 1.2 completed

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,98 @@
-alert('Hello, World!');
+let array = [];
+let pokemonList = array;
 
-let favoriteFood = 'pizza';
-document.write(favoriteFood);
+array = [{
+    name: 'Bulbasaur',
+    heigth: .7,
+    type: ['grass', 'poison']
+  },
+  {
+    name: 'Ivysaur',
+    heigth: 1,
+    type: ['grass', 'poison']
+  },
+  {
+    name: 'Charizard',
+    heigth: 1.7,
+    type: ['fire', 'flying']
+  },
+  {
+    name: 'Blastoise',
+    heigth: 1.6,
+    type: ['water']
+  },
+  {
+    name: 'Ekans',
+    heigth: 2,
+    type: ['poison']
+  },
+  {
+    name: 'Pikachu',
+    heigth: 0.4,
+    type: ['electric']
+  },
+  {
+    name: 'Arbok',
+    heigth: 3.5,
+    type: ['poison']
+  },
+  {
+    name: 'Golurk',
+    heigth: 12.8,
+    type: ['ghost', 'ground']
+  },
+  {
+    name: 'Clefable',
+    heigth: 1.3,
+    type: ['fairy']
+  },
+  {
+    name: 'Venomoth',
+    heigth: 1.5,
+    type: ['bug', 'poison']
+  },
+  {
+    name: 'Abra',
+    heigth: .9,
+    type: ['psychic']
+  },
+  {
+    name: 'Machamp',
+    heigth: 1.6,
+    type: ['fighting']
+  },
+  {
+    name: 'Slowpoke',
+    heigth: 1.2,
+    type: ['psychic', 'water']
+  },
+  {
+    name: 'Chansei',
+    heigth: 1.1,
+    type: ['normal']
+  },
+  {
+    name: 'Forretrees',
+    heigth: 1.2,
+    type: ['steel', 'bug']
+  },
+  {
+    name: 'Palpitoad',
+    heigth: .8,
+    type: ['water', 'ground']
+  },
+];
 
-let myName = 'Wojtek';
-document.write(myName);
-myName = 'Tomek';
-document.write(myName);
+//just playing
+console.log(pokemonList);
+console.log(`Number of Pokemons: ${array.length}`);
+console.log(array[2]);
+console.log(array[2].name);
+console.log(array[0].type);
+console.log(array[0].type[1]);
 
-let simpleAddition = 2 + 2;
-document.write(simpleAddition);
-
-let size = 100;
-let doubleSize = size * 2;
-document.write(doubleSize);
-
-let minSize = (doubleSize * 2) - (doubleSize / 2);
-document.write(minSize);
+document.querySelector('h1').innerHTML = (`Number of Pokemons: ${array.length}`);
+for (let i = 0; i < (array.length); i++) {
+  console.log(array[i].name);
+  document.write(`${array[i].name} <br>`);
+}
