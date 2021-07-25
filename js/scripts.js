ceca30a7de634da25d14cff1f4b8c8313a765f54
wojtek-lukowski <75492008+wojtek-lukowski@users.noreply.github.com>
2021-07-25 14:25:08 +0200

    index/js/css

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+let myName = 'Wojtek';
+document.write(myName);
+myName = 'Tomek';
+document.write(myName);
+
+let simpleAddition = 2 + 2;
+document.write(simpleAddition);
+
+let size = 100;
+let doubleSize = size * 2;
+document.write(doubleSize);
+
+let minSize = (doubleSize * 2) - (doubleSize / 2);
+document.write(minSize);
